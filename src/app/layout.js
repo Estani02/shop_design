a9f12b6a74b0af554304e1420ee6b265dc673bbc
Estani02 <estaniolmedo@gmail.com>
2023-09-20 16:07:38 -0300

    added cookie

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 import { Manrope } from 'next/font/google'
 import { Footer } from '@/components/Footer'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
+import CookieBanner from '@/components/Cookiebanner'
 
 const manrope = Manrope({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className={manrope.className}>
         <Header />
         {children}
+        <CookieBanner />
         <Footer />
       </body>
     </html>

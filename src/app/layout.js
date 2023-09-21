e0c023b207f5bd3c8904a5150f571ceb89fdfa7e
Estani02 <estaniolmedo@gmail.com>
2023-09-21 08:38:36 -0300

    out cookies

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       <body className={manrope.className}>
         <Header />
         {children}
-        <CookieBanner />
+        {/* <CookieBanner /> */}
         <Footer />
       </body>
     </html>
